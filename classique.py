6f74344feb1d90bf9e44365a009e2f7972a07ede
sergeLabo <sergeblender@gmail.com>
2015-03-09 11:36:01 +0100

    ajout des dossiers

---
+++ classique.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import kivy
+kivy.require('1.8.0')
+
+from kivy.app import App
+from kivy.core.window import Window
+
+Window.size = (1120, 630)
+
+class ClassiqueApp(App):
+    def build(FloatLayout):
+        pass
+
+if __name__ == '__main__':
+    ClassiqueApp().run()
